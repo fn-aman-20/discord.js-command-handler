e266ee0dc4d7d31e9305ca33724f40be228a5464
Aman <99836388+fn-aman-20@users.noreply.github.com>
2022-11-22 06:45:20 +0530

    Create counter.js

---
+++ commands/counter.js
@@ -0,0 +1,14 @@
+const { SlashCommandBuilder } = require('@discordjs/builders'),
+{ ChannelType } = require('discord.js');
+
+module.exports = new SlashCommandBuilder()
+.setName('counter')
+.setDescription('configure counter settings')
+.addChannelOption(channel => channel
+  .setName('channel')
+  .setDescription('select a channel to enable/disable the counter')
+  .addChannelTypes(ChannelType.GuildText)
+  .setRequired(true))
+.addIntegerOption(count => count
+  .setName('count')
+  .setDescription(`update the counter's count`))
