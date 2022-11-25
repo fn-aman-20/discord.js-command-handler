e45d5c9b0e6e15f1c257ec150417a554cf59b854
Aman <99836388+fn-aman-20@users.noreply.github.com>
2022-11-25 15:14:22 +0530

    main

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
-const host = require('express')();
-host.get('/', (req, res) => res.send(''));
-host.listen(2022);
+/* const host = require('express')();
+host.get('/', (req, res) => res.send('Thank you for the ping!'));
+host.listen(2022); tranform this comment into code if you are using replit and are going to ping it */
 
 const {
   Client,
+  Options,
   ActivityType,
   GatewayIntentBits
 } = require('discord.js');
@@ -12,17 +13,48 @@ const client = new Client({
   intents: Object.values(GatewayIntentBits)
     .filter(perm => typeof perm === 'number'),
   shards: 'auto',
+  // remove the entire makeCache property if you don't want it, more documentation on it below:
+  // https://discord.js.org/#/docs/discord.js/main/typedef/ClientOptions
+  // https://discord.js.org/#/docs/discord.js/main/class/Options?scrollTo=s-cacheWithLimits
+  makeCache: Options.cacheWithLimits({
+    GuildBanManager: 5,
+    GuildEmojiManager: 10,
+    GuildForumThreadManager: 1,
+    GuildInviteManager: 5,
+    GuildMemberManager: {
+      maxSize: 10,
+      keepOverLimit: member => member.id === client.user.id
+    },
+    GuildScheduledEventManager: 1,
+    GuildStickerManager: 5,
+    GuildTextThreadManager: 1,
+    BaseGuildEmojiManager: 10,
+    UserManager: 0,
+    ThreadManager: 0,
+    MessageManager: 0,
+    PresenceManager: 0,
+    VoiceStateManager: 0,
+    ThreadMemberManager: 0,
+    ReactionUserManager: 0,
+    ReactionManager: 0,
+    StageInstanceManager: 0,
+    ApplicationCommandManager: 0
+  }),
   presence: {
     status: 'idle',
     afk: false,
     activities: [{
-      name: 'Something',
-      type: ActivityType.Streaming,
+      name: 'My Own Damn Self',
+      type: ActivityType.Watching,
       url: 'https://twitch.tv/nocopyrightsounds'
     }]
   }
 });
 
+// remove line 54 - 57 if you didn't use the makeCache prop in client
+delete client.sweepers.options;
+clearInterval(client.sweepers.intervals.threads);
+client.sweepers.intervals.threads = null;
 client.config = require('./util/config');
 client.db = require('croxydb');
 client.db.setReadable(true);
@@ -32,4 +64,4 @@ require('fs').readdirSync('./events').filter(f => f.endsWith('.js')).forEach(e =
   client.on(`${event}`, task.bind(null, client));
 });
 
-client.config.onboot(client, client.config.auth);
+// client.config.onboot(client, client.config.auth);
