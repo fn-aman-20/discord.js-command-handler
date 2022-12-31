3969548c661423feb30444fd46e54a7c3a487182
Aman <99836388+fn-aman-20@users.noreply.github.com>
2022-12-31 17:24:42 +0530

    Update interactionCreate.js

--- events/interactionCreate.js
+++ events/interactionCreate.js
@@ -1,46 +1,9 @@
 module.exports = async (client, i) => {
-  if (!i.member.permissions.toArray().includes('Administrator')) return await i.reply({
-    content: `Sorry, I agree with Admins for my configuration.`
-  });
-  if (i.isCommand()) {
-    if (i.commandName === 'counter') {
-      await i.deferReply({ ephemeral: true });
-      const channel = i.options.getChannel('channel'),
-      count = i.options.getInteger('count');
-      let data = client.db.get(i.guild.id);
-      if (!data) {
-        data = {};
-        data[channel.id] = count || 0;
-        client.db.set(i.guild.id, data);
-        return await i.editReply({
-          content: `✅ You shall start with ${data[channel.id] + 1} in ${channel.name}`
-        });
-      }
-      if (count) {
-        if (count < 0) return await i.editReply({
-          content: `**Be Positive Please!**`
-        });
-        data[channel.id] = count;
-        client.db.set(i.guild.id, data);
-        return await i.editReply({
-          content: `✅ You shall start with ${count + 1} in ${channel.name}`
-        });
-      }
-      else if (!count) {
-        if (!data[channel.id]) {
-          data[channel.id] = 0;
-          client.db.set(i.guild.id, data);
-          return await i.editReply({
-            content: `✅ You shall start with 1 in ${channel.name}`
-          });
-        } else if (data[channel.id]) {
-          delete data[channel.id];
-          client.db.set(i.guild.id, data);
-          return await i.editReply({
-            content: `✅ No one can count in ${channel.name} anymore`
-          });
-        }
-      }
-    }
-  }
+  if (!i.guild || i.user.bot) return;
+  (async () => { // one block for all of your chat input commands!
+    let command = client.commands.get(i.commandName);
+    if (!command) return;
+    try { await command.run(client, i) } catch { return };
+  })();
+  // more code here
 }
