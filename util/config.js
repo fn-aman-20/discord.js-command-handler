2f40034729dcb86c1c53e2e5d71208ea8665fc6d
Sanjeev Sahoo <99836388+arm-aman@users.noreply.github.com>
2023-01-04 14:16:27 +0530

    Update config.js

--- util/config.js
+++ util/config.js
@@ -3,4 +3,5 @@ const onboot = require('./onboot');
 module.exports = {
   auth: 'Your Token Here',
   onboot: onboot
+  // other props you'd like to have, you can acces these later from client.config
 }
